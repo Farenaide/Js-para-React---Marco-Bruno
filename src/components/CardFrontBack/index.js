eb0618fb683e62a092cdfec333312333196350f5
Alan <aalan.lima@live.com>
2023-03-20 13:35:21 -0300

    Add click event on CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,9 +1,19 @@
 import CardGame from "../CardGame";
 import "./style.css"
 
+
+
+
 const CardFrontBack = () => {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event)=>{
+        const $target = event.target;
+        const $toFlip = $target.closest('.card-front-back');
+        $toFlip.classList.toggle('_flipped');
+    }
+   
     return /* html */`
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
 
             <div class="card _front">
                 ${CardGame()}
